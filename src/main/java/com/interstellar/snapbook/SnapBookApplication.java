2bd5c006907df1235d1d1d8a19f65cf60bae10ef
namjeong-yoo <skawjddb@gmail.com>
2026-05-02 15:20:23 +0900

    Initial commit

---
+++ src/main/java/com/interstellar/snapbook/SnapBookApplication.java
@@ -0,0 +1,13 @@
+package com.interstellar.snapbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interstellar/snapbook/SnapBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interstellar.snapbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
